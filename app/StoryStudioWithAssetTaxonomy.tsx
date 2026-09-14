acaec0c5339bbd904498e373099edbd1be2ac711
LUCKYBRIDGE <lucky20220528@gmail.com>
2026-09-14 23:41:33 +0900

    feat: expose rabbit classic reading from library

--- app/StoryStudioWithAssetTaxonomy.tsx
+++ app/StoryStudioWithAssetTaxonomy.tsx
@@ -3,10 +3,16 @@
 import { StoryStudio } from "./StoryStudio";
 import { STORY_ASSETS } from "./story-assets";
 import { applyStoryAssetTaxonomy } from "./story-asset-taxonomy";
+import { ClassicReadingLibraryEntry } from "./components/ClassicReadingLibraryEntry";
 
 // 기존 StoryAsset ID와 저장 형식은 유지하고, 이미지 Picker가 읽는 태그만 보강한다.
 applyStoryAssetTaxonomy(STORY_ASSETS);
 
 export function StoryStudioWithAssetTaxonomy() {
-  return <StoryStudio />;
+  return (
+    <>
+      <StoryStudio />
+      <ClassicReadingLibraryEntry />
+    </>
+  );
 }
